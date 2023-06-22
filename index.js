73a41eebb097a9335ae70c4c2fa0aba42a238cbd
Stanislav <bassketb86@gmail.com>
2023-06-22 10:46:13 +0300

    f

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.get("/api/:name", (req, res) => {
   res.json({ message: `Hello ${req.params.name}, from server!` });
 });
-
+// if u have no env file - del
 app.listen(process.env.PORT || 3001, () => {
-  console.log(`Server listening on ${process.env.PORT || 3001}`);
+  console.log(`Server listening on ${process.env.PORT || 3001}` RIGHT NOW);
 });
